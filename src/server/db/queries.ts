2fd1052051a15b0c8d5bd8e8339a73e591802879
Daniel-Burbridge-Developer <daniel.burbridge.developer@gmail.com>
2024-11-22 22:22:27 +0800

    preparing to get random quotes

--- src/server/db/queries.ts
+++
@@ -1,7 +0,0 @@
-export async function getRandomQuote() {
-  const res = await fetch("https://zenquotes.io/api/random/", {
-    method: "GET",
-  });
-  const data = await res.json();
-  return data.content;
-}
